1d5195531da068940227699cf6f4fa90172e3ee3
Raghu K <raghuk.vit@gmail.com>
2022-07-07 07:38:15 +0530

    Update roboshop-infra/all-modules/tf-modfule-immutable/

--- alb.tf
+++ alb.tf
@@ -1,19 +1,19 @@
-//resource "aws_lb_target_group" "target-group" {
-//  name     = "${var.COMPONENT}-${var.ENV}-tg"
-//  port     = var.PORT
-//  protocol = "HTTP"
-//  vpc_id   = var.VPC_ID
-//  health_check {
-//    enabled             = true
-//    healthy_threshold   = 3
-//    unhealthy_threshold = 2
-//    interval            = 5
-//    timeout             = 4
-//    path                = "/health"
-//    port                = var.PORT
-//  }
-//}
-//
+resource "aws_lb_target_group" "target-group" {
+  name     = "${var.COMPONENT}-${var.ENV}-tg"
+  port     = var.PORT
+  protocol = "HTTP"
+  vpc_id   = var.VPC_ID
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    interval            = 5
+    timeout             = 4
+    path                = "/health"
+    port                = var.PORT
+  }
+}
+
 //resource "aws_lb_target_group_attachment" "instances-to-tg" {
 //  count            = length(local.ALL_INSTANCE_IDS)
 //  target_group_arn = aws_lb_target_group.target-group.arn
