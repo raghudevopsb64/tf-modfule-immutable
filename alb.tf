9bbed7d91a905d44d21e69ab3c04b08b64a6623f
Raghu K <raghuk.vit@gmail.com>
2022-07-07 07:43:04 +0530

    Update roboshop-infra/all-modules/tf-modfule-immutable/

--- alb.tf
+++ alb.tf
@@ -14,34 +14,28 @@ resource "aws_lb_target_group" "target-group" {
   }
 }
 
-//resource "aws_lb_target_group_attachment" "instances-to-tg" {
-//  count            = length(local.ALL_INSTANCE_IDS)
-//  target_group_arn = aws_lb_target_group.target-group.arn
-//  target_id        = element(local.ALL_INSTANCE_IDS, count.index)
-//  port             = var.PORT
-//}
-//
-//resource "aws_lb" "alb" {
-//  name               = "${var.COMPONENT}-${var.ENV}-alb"
-//  internal           = var.IS_ALB_INTERNAL
-//  load_balancer_type = "application"
-//  security_groups    = [aws_security_group.alb.id]
-//  subnets            = var.SUBNET_IDS
-//
-//  tags = {
-//    Name = "${var.COMPONENT}-${var.ENV}-alb"
-//    ENV  = var.ENV
-//  }
-//}
-//
-//resource "aws_lb_listener" "component-http" {
-//  load_balancer_arn = aws_lb.alb.arn
-//  port              = "80"
-//  protocol          = "HTTP"
-//
-//  default_action {
-//    type             = "forward"
-//    target_group_arn = aws_lb_target_group.target-group.arn
-//  }
-//}
-//
+
+resource "aws_lb" "alb" {
+  name               = "${var.COMPONENT}-${var.ENV}-alb"
+  internal           = var.IS_ALB_INTERNAL
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = var.SUBNET_IDS
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}-alb"
+    ENV  = var.ENV
+  }
+}
+
+resource "aws_lb_listener" "component-http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
+  }
+}
+
