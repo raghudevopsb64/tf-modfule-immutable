45c46bf10793830045c1a740cf10cb383b901f25
Raghu K <raghuk.vit@gmail.com>
2022-07-07 08:34:08 +0530

    Update roboshop-infra/all-modules/tf-modfule-immutable/

--- user-data-set.tf
+++ user-data-set.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "user-data-script" {
   provisioner "local-exec" {
-    command = "sed -i -e \"s|DOCUMENTDB_ENDPOINT|${var.DOCDB_ENDPOINT}|\" ${path.module}/${var.ENV}-userdata.sh"
+    command = "sed -i -e 's|DOCUMENTDB_ENDPOINT|${var.DOCDB_ENDPOINT}|' ${path.module}/${var.ENV}-userdata.sh"
   }
 }
